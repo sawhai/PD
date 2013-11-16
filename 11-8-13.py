e4f2536cd04b3947e592365a9f5937a301056e0d
Michael Bommarito <michael.bommarito@solidlogic.com>
2013-11-16 08:37:02 -0500

    Preparing Aristos's assignment for the class's next assignment

--- 11-8-13.py
+++ 11-8-13.py
@@ -1,71 +1,108 @@
-import random
+'''
+Simple PD game model.
+'''
+
+import numpy.random
 
 class SimpleGame(object):
+    '''
+    Simple game class.
+    This class is analogous to our Tournament object in the 
+    Rock-Paper-Scissors tournament.
+    '''
     
-    payoffMatrix = [ [(3,3), (0,5)] , [(5,0), (1,1)] ]
+    # Payoff matrix; 
+    payoff_matrix = [[(3, 3), (0, 5)],
+                    [(5, 0), (1, 1)]]
     
-    history = []
+    # Game history
+    game_history = []
     
-    def __init__(self, playerList):
+    def __init__(self, player_list):
+        '''
+        Constructor for the game; takes a list of players.
         
-        self.players = playerList
+        You need to make sure that any variables, e.g., player_list,
+        are made available to the class ongoing.
+        '''
+        pass
         
     def payoff(self, moves):
+        '''
+        Payoff method given a set of moves.
         
-        return self.payoffMatrix[moves[0]][moves[1]]
+        This method needs to return the payoff value for a given 
+        pair of moves; you should use the payoff_matrix defined 
+        above.
+        '''
+        pass
         
     def run(self):
+        '''
+        Run method to run an actual game.
         
-        outcome = [self.players[0].move(),self.players[1].move()]
-        
-        scores = self.payoff(outcome)
-        
-        self.players[0].record(outcome,scores)
-        
-        self.players[1].record(outcome,scores)
-        
-        self.history.append([outcome,scores])
-        
-        return [outcome, scores]
-
+        You will need to do the following:
+            1. Ask players for their moves
+            2. Score the moves to determine their payoffs
+            3. Update their scores
+            4. Record their histories
+        '''
+        pass
+    
 class RandomPlayer(object):
+    '''
+    RandomPlayer class is a simple PD player who
+    has a constant probability of defecting per game.
+    '''
     
-    p_defect = 0.5
+    # Default probability of defection 
+    probability_defect = 0.5
     
-    game = []
+    # Score
+    score = 0.0
     
-    def __init__(self,pd):
+    # History
+    history = []
+    
+    def __init__(self, probability_defect):
+        '''
+        Constructor for the player; takes a probability
+        of defection as input.
         
-        self.p_defect = pd
+        You will need to set the class variable from the argument.
+        '''
+        pass
         
     def move(self):
+        '''
+        The move method returns the player's move based on a
+        random draw and their constant probability.
         
-        r = random.random()
-        
-        if r < self.p_defect:
-            
-            return 0
-            
-        else:
-            
-            return 1
-    
+        You will need to:
+            1. Draw a random variate from numpy.random.random()
+            2. Transform the value into an action
+        '''
+        pass
+
     def record(self, outcome, score):
-        
+        '''
+        The record method allows the player to update their score
+        and history values if desired.
+        '''
         pass
     
 
+if __name__ == "__main__":
+    '''
+    Sample driver.
+    '''
+    # Create players
+    player_1 = RandomPlayer(0.5)
+    player_2 = RandomPlayer(0.25)
+    
+    # Create game
+    simple_game = SimpleGame([player_1, player_2])
 
-player1 = RandomPlayer(0.5)
-player2 = RandomPlayer(0.25)
-SG = SimpleGame([player1,player2])
-
-result = SG.run()
-
-print result
-
-SG2 = SimpleGame([player1,player2])
-
-for t in range(100):
-    SG2.run()
-print SG2.history
+    # Run the game
+    result = simple_game.run()
+    print result
